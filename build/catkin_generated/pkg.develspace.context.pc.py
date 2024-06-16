400c7a5ee36bd0c2927d70dd33d530a71f604876
huseyin <katlakoy@outlook.com>
2024-06-16 10:01:07 +0300

    nochange

---
+++ build/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "roscpp;gazebo_ros;gazebo_ros_control;controller_manager;effort_controllers;robot_state_publisher;std_msgs;sensor_msgs".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "tekno"
+PROJECT_SPACE_DIR = "/home/huseyin/tekno_ws/src/tekno/build/devel"
+PROJECT_VERSION = "0.0.0"
