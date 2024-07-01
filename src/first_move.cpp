350a8adae7e7e0632fccb121611e52f071d0fb84
huseyin <katlakoy@outlook.com>
2024-07-01 19:48:35 +0300

    total iş

--- src/first_move.cpp
+++ src/first_move.cpp
@@ -1,4 +1,11 @@
 #include <fstream>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <thread>
+#include <mutex>
+
+#include <fstream>
 #include <iostream>
 #include <sstream>
 #include <ros/ros.h>
@@ -21,6 +28,57 @@
 #define MIN_ANGULAR_VELOCITY -1
 
 double left_wheel_velocity, right_wheel_velocity;
+geometry_msgs::Twist command_vel;
+std::mutex vel_mutex;
+
+
+char getch() {
+    char buf = 0;
+    struct termios old = {0};
+    if (tcgetattr(0, &old) < 0)
+        perror("tcsetattr()");
+    old.c_lflag &= ~ICANON;
+    old.c_lflag &= ~ECHO;
+    old.c_cc[VMIN] = 1;
+    old.c_cc[VTIME] = 0;
+    if (tcsetattr(0, TCSANOW, &old) < 0)
+        perror("tcsetattr ICANON");
+    if (read(0, &buf, 1) < 0)
+        perror ("read()");
+    old.c_lflag |= ICANON;
+    old.c_lflag |= ECHO;
+    if (tcsetattr(0, TCSADRAIN, &old) < 0)
+        perror ("tcsetattr ~ICANON");
+    return (buf);
+}
+
+void keyboardLoop() {
+    char ch;
+    while (ros::ok()) {
+        ch = getch();  // Get the pressed key
+        std::lock_guard<std::mutex> lock(vel_mutex);  // Lock the mutex to modify command_vel safely
+        if (ch == 27) {  // ESC key (27) to terminate
+            break;
+        } else if (ch == 'w') {  // Forward
+            command_vel.linear.x = 1;
+            command_vel.angular.z = 0;
+        } else if (ch == 's') {  // Backward
+            command_vel.linear.x = -1;
+            command_vel.angular.z = 0;
+        } else if (ch == 'a') {  // Turn left
+            command_vel.linear.x = 0;
+            command_vel.angular.z = 1;
+        } else if (ch == 'd') {  // Turn right
+            command_vel.linear.x = 0;
+            command_vel.angular.z = -1;
+        } else if (ch == ' ') {  // Stop
+            command_vel.linear.x = 0;
+            command_vel.angular.z = 0;
+        }
+    }
+}
+
+
 
 void LinkStatesCb(const gazebo_msgs::LinkStates::ConstPtr &msg) {
     for (int i = 0; i < msg->name.size(); i++) {
@@ -87,6 +145,9 @@ int main(int argc, char **argv) {
         task_file.close();
     }
 
+    std::srand(static_cast<unsigned int>(time(nullptr))); // Random number seed
+
+    std::thread keyboard_thread(keyboardLoop);  // Start the keyboard input thread
 
     geometry_msgs::Twist command_vel;
     
@@ -115,30 +176,39 @@ int main(int argc, char **argv) {
         // double radian = (counter * M_PI) / 180.0;
         // double hiz = sin(radian);
 
-        command_vel.linear.x = 1;
-        command_vel.angular.z = 0;
+        // command_vel.linear.x = 1;
+        // command_vel.angular.z = 0;
 
         
-        double aci =  atan((  5- robot_position.y) / ( 7 - robot_position.x))  ;
+        // double aci =  atan((  5- robot_position.y) / ( 7 - robot_position.x))  ;
         
 
-        if ( aci < yaw + 0.1 ){
-            command_vel.angular.z = 0.5 ;
-            command_vel.linear.x = 0;
-            ROS_INFO("aci: %f, %f", aci, yaw);   
+        // if ( aci < yaw + 0.1 ){
+        //     command_vel.angular.z = 0.5 ;
+        //     command_vel.linear.x = 0;
+        //     ROS_INFO("aci: %f, %f", aci, yaw);   
         
-            ROS_INFO("Current robot position: %f, %f, %f", robot_position.x, robot_position.y, robot_position.z);
-            ROS_INFO("Current left-right wheel angular velocities: %f, %f", left_wheel_velocity, right_wheel_velocity);
-            ROS_INFO("New velocity targets: %f, %f", command_vel.linear.x, command_vel.angular.z);
-            ROS_INFO("NEW angular things: %f, %f, %f", roll, pitch, yaw);
+        //     ROS_INFO("Current robot position: %f, %f, %f", robot_position.x, robot_position.y, robot_position.z);
+        //     ROS_INFO("Current left-right wheel angular velocities: %f, %f", left_wheel_velocity, right_wheel_velocity);
+        //     ROS_INFO("New velocity targets: %f, %f", command_vel.linear.x, command_vel.angular.z);
+        //     ROS_INFO("NEW angular things: %f, %f, %f", roll, pitch, yaw);
             
-        }   
+        // }   
                 
-        command_velocity_publisher.publish(command_vel);                        
+        // command_velocity_publisher.publish(command_vel);                        
+        // counter ++ ;
+        
+        {
+            std::lock_guard<std::mutex> lock(vel_mutex);  // Lock the mutex to access command_vel safely
+            command_velocity_publisher.publish(command_vel);
+        }
+
         ros::spinOnce();
         control_rate.sleep();
-        counter ++ ;
+
     }
+
+    keyboard_thread.join();  // Wait for the keyboard input thread to finish
     
     return 0;
 }
